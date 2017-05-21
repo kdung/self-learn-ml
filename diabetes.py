5a5dc3bd3ed853d3828b039f5b5fe604ba1a4e5c
kim <kdung@users.noreply.github.com>
2017-05-21 15:40:08 +0800

    Add files via upload

--- diabetes.py
+++ diabetes.py
@@ -39,10 +39,10 @@ from sklearn.neural_network import MLPClassifier
 mlp = MLPClassifier(hidden_layer_sizes=(100), max_iter=1000, verbose=True)
 
 mlp.fit(X_train, y_train)
-# loss =  0.26141378
+# loss = 0.23052485
 train_predictions = mlp.predict(X_train)
 train_accuracy = (train_predictions == y_train).sum()/len(y_train)
-print('train accuracy %s' % train_accuracy) #0.90625
+print('train accuracy %s' % train_accuracy) #0.93055555555555558
 '''
 MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
        beta_2=0.999, early_stopping=False, epsilon=1e-08,
@@ -59,19 +59,28 @@ predictions = mlp.predict(X_test)
 from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test, predictions))
 '''
-[[107  16]
- [ 19  50]]
+[[105  18]
+ [ 22  47]]
 '''
 print(classification_report(y_test, predictions))
 '''
                 precision    recall  f1-score   support
 
-          0       0.85      0.87      0.86       123
-          1       0.76      0.72      0.74        69
+         0       0.83      0.85      0.84       123
+          1       0.72      0.68      0.70        69
 
-avg / total       0.82      0.82      0.82       192
+avg / total       0.79      0.79      0.79       192
 '''
 
 len(mlp.coefs_) #4
 len(mlp.coefs_[0]) #11
 len(mlp.intercepts_[0]) #25
+
+# plot learning curve
+from sklearn.model_selection import ShuffleSplit
+from plot_learning_curve import plot_learning_curve
+title = "Learning Curves"
+estimator = MLPClassifier(hidden_layer_sizes=(100), max_iter=1000)
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+plt = plot_learning_curve(estimator, title, X_train, y_train, (0.0, 1.01), cv=cv, n_jobs=1)
+plt.show()
