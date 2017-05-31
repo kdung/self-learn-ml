a3873922cb9da120cfaccf795ab43c08b4e5da24
kim <kdung@users.noreply.github.com>
2017-05-31 17:04:12 +0800

    train diabetes dataset with sklearn

--- diabetes.py
+++ diabetes.py
@@ -10,7 +10,7 @@ import pandas as pd
 diabetes = pd.read_csv('Diabetes.csv')
 diabetes.head()
 diabetes.describe().transpose()
-diabetes.shape #(4898, 12)
+diabetes.shape
 X = diabetes.drop('Classify', axis=1)
 y = diabetes['Classify']
 
@@ -36,13 +36,13 @@ X_test = scaler.transform(X_test)
 
 from sklearn.neural_network import MLPClassifier
 
-mlp = MLPClassifier(hidden_layer_sizes=(100), max_iter=1000, verbose=True)
+mlp = MLPClassifier(hidden_layer_sizes=(8), max_iter=1000, verbose=False)
 
 mlp.fit(X_train, y_train)
 # loss = 0.23052485
 train_predictions = mlp.predict(X_train)
 train_accuracy = (train_predictions == y_train).sum()/len(y_train)
-print('train accuracy %s' % train_accuracy) #0.93055555555555558
+print('train accuracy %s' % train_accuracy) #0.93055555555555558 0.973958333333
 '''
 MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
        beta_2=0.999, early_stopping=False, epsilon=1e-08,
@@ -55,21 +55,26 @@ MLPClassifier(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
 
 # prediction and evaluation
 predictions = mlp.predict(X_test)
-
+test_accuracy = (predictions == y_test).sum()/len(y_test)
+print('test accuracy %s' % test_accuracy) #0.755208333333
+'''
+train accuracy 0.765625
+test accuracy 0.807291666667
+'''
 from sklearn.metrics import classification_report, confusion_matrix
 print(confusion_matrix(y_test, predictions))
 '''
-[[105  18]
- [ 22  47]]
+[[107  16]
+ [ 21  48]]
 '''
 print(classification_report(y_test, predictions))
 '''
                 precision    recall  f1-score   support
 
-         0       0.83      0.85      0.84       123
-          1       0.72      0.68      0.70        69
+          0       0.84      0.87      0.85       123
+          1       0.75      0.70      0.72        69
 
-avg / total       0.79      0.79      0.79       192
+avg / total       0.81      0.81      0.81       192
 '''
 
 len(mlp.coefs_) #4
@@ -77,10 +82,11 @@ len(mlp.coefs_[0]) #11
 len(mlp.intercepts_[0]) #25
 
 # plot learning curve
+"""
 from sklearn.model_selection import ShuffleSplit
 from plot_learning_curve import plot_learning_curve
 title = "Learning Curves"
-estimator = MLPClassifier(hidden_layer_sizes=(100), max_iter=1000)
 cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-plt = plot_learning_curve(estimator, title, X_train, y_train, (0.0, 1.01), cv=cv, n_jobs=1)
+plt = plot_learning_curve(mlp, title, X_train, y_train, (0.0, 1.01), cv=cv, n_jobs=1)
 plt.show()
+"""
